291d6f4df2c6e44572f3d90b7b59e7decce175dc
chaoqi <40240594+chaoqi7@users.noreply.github.com>
2019-11-24 15:09:03 +0800

    Update client.cpp
    客户端引入 select , 输入线程

--- src/EasyTcpClient/client.cpp
+++ src/EasyTcpClient/client.cpp
@@ -8,11 +8,88 @@
 #pragma comment(lib, "ws2_32.lib")
 #else
 //linux and osx
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+typedef int SOCKET
 #endif // _WIN32
 
 #include <stdio.h>
+#include <thread>
 
 #include "NetMsg.h"
+
+bool g_bRun = true;
+
+int processor(SOCKET cSock)
+{
+	char msgBuf[1024] = {};
+	int nLen = recv(cSock, msgBuf, sizeof(DataHeader), 0);
+	if (nLen <= 0)
+	{
+		printf("nLen=%d <sock=%d>与服务器断开连接，任务结束.\n", nLen, cSock);
+		return -1;
+	}
+
+	DataHeader* pDh = (DataHeader*)msgBuf;
+	switch (pDh->cmd)
+	{
+	case CMD_LOGIN_RESULT:
+	{
+		recv(cSock, msgBuf + sizeof(DataHeader), pDh->dataLength - sizeof(DataHeader), 0);
+		LoginResult* pLoginResult = (LoginResult*)msgBuf;
+		printf("收到服务器返回消息 CMD_LOGIN_RESULT, Result:%d, len:%d\n",
+			pLoginResult->result, pLoginResult->dataLength);
+	}
+	break;
+	case CMD_LOGINOUT_RESULT:
+	{
+		recv(cSock, msgBuf + sizeof(DataHeader), pDh->dataLength - sizeof(DataHeader), 0);
+		LogoutResult* pLogoutResult = (LogoutResult*)msgBuf;
+		printf("收到服务器返回消息 CMD_LOGINOUT_RESULT, Result:%d, len:%d\n", 
+			pLogoutResult->result, pLogoutResult->dataLength);
+	}
+	break;
+	case CMD_NEW_USER_JOIN:
+	{
+		recv(cSock, msgBuf + sizeof(DataHeader), pDh->dataLength - sizeof(DataHeader), 0);
+		NewUserJoin* pUserJoin = (NewUserJoin*)msgBuf;
+		printf("收到服务器返回消息 CMD_NEW_USER_JOIN, sock:%d, len:%d\n", 
+			pUserJoin->sock, pUserJoin->dataLength);
+	}
+	break;
+	default:
+		break;
+	}
+}
+
+void cmdThread(SOCKET sock)
+{
+	while (true)
+	{
+		char cmdBuf[256] = {};
+		scanf("%s", cmdBuf);
+		if (0 == strcmp(cmdBuf, "exit"))
+		{
+			printf("cmdThread need exit.\n");
+			g_bRun = false;
+			break;
+		}
+		else if (0 == strcmp(cmdBuf, "login")) {
+			Login login = {};
+			strcpy(login.userName, "chaoqi");
+			strcpy(login.passWord, "chaoqimima");
+			send(sock, (const char*)&login, sizeof(Login), 0);
+		}
+		else if (0 == strcmp(cmdBuf, "logout")) {
+			Logout logout = {};
+			strcpy(logout.userName, "chaoqi");
+			send(sock, (const char*)&logout, sizeof(Logout), 0);
+		}
+		else {
+			printf("unknown command, input again.\n");
+		}
+	}	
+}
 
 int main(int argc, char** argv)
 {
@@ -44,39 +121,34 @@ int main(int argc, char** argv)
 		printf("连接服务器成功.\n");
 	}
 
-	while (true)
+	std::thread cmdThread(cmdThread, _sock);
+	cmdThread.detach();
+
+	while (g_bRun)
 	{
-		//输入请求命令
-		char cmdBuf[128] = {};
-		scanf("%s", cmdBuf);	
-		//处理请求
-		if (0 == strcmp(cmdBuf, "exit"))
+		fd_set fdReads;
+		FD_ZERO(&fdReads);
+		FD_SET(_sock, &fdReads);
+		timeval t = { 1, 0 };
+		int ret = select(_sock, &fdReads, nullptr, nullptr, &t);
+		if (ret == SOCKET_ERROR)
 		{
+			printf("select error.\n");
 			break;
-		} 
-		else if (0 == strcmp(cmdBuf, "login")) {					
-			Login login;
-			strcpy(login.userName, "chaoqi");
-			strcpy(login.passWord, "mima");
-			//向服务器发送指令
-			send(_sock, (const char*)&login, sizeof(login), 0);
-			//接收服务器返回的指令
-			LoginResult loginResult = {};
-			recv(_sock, (char*)&loginResult, sizeof(LoginResult), 0);
-			printf("返回登录结果: %d.\n", loginResult.result);
 		}
-		else if (0 == strcmp(cmdBuf, "logout")) {
-			Logout logout;
-			strcpy(logout.userName, "chaoqi");
-			//向服务器发送指令
-			send(_sock, (const char*)&logout, sizeof(Logout), 0);
-			//接收服务器返回的指令
-			LogoutResult logoutResult = {};
-			recv(_sock, (char*)&logoutResult, sizeof(LogoutResult), 0);
-			printf("返回登出结果: %d.\n", logoutResult.result);
+		else if (ret == 0) {
+			//printf("select time out.\n");
+			//continue;
 		}
-		else {
-			printf("不识别的指令，请重新输入.\n");
+
+		if (FD_ISSET(_sock, &fdReads))
+		{
+			FD_CLR(_sock, &fdReads);
+			if (-1 == processor(_sock))
+			{
+				printf("select processor error.\n");
+				break;
+			}
 		}
 	}
 
