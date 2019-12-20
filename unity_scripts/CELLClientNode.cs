73296ab579c2c0b0adb88f62fcbe442579d15e96
chaoqi <40240594+chaoqi7@users.noreply.github.com>
2019-12-20 16:46:22 +0800

    增加 unity 通信脚本

---
+++ unity_scripts/CELLClientNode.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CELLClientNode : CELLNativeTCPClient
+{
+    public string IP = "127.0.0.1";
+    public short PORT = 4567;
+    private bool _bConnect = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Create();
+        if (this.Connect(IP, PORT))
+        {
+            Debug.Log("connect to server success.");
+            _bConnect = true;
+        }
+        else
+        {
+            Debug.Log("connect to server fail.");
+        }
+
+        CELLSendStream w = new CELLSendStream();
+        w.WriteNetCMD(NetCMD.C2S_STREAM);
+        w.WriteInt8(1);
+        w.WriteInt16(2);
+        w.WriteInt32(3);
+        w.WriteInt64(4);
+        w.WriteUInt16(5);
+        w.WriteFloat(6.7f);
+        w.WriteDouble(8.9);
+
+        w.WriteString("client");
+        w.WriteString("haha");
+        int[] arr2 = { 10, 11, 12, 13, 15 };
+        w.WriteInts(arr2);
+        w.Finish();
+
+        this.SendData(w.Array);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_bConnect)
+        {
+            this.OnRun();
+        }
+        
+        //SendData()
+    }
+
+    //     void OnDestroy()
+    //     {
+    //         if (_bConnect)
+    //         {
+    //             Debug.Log("OnDestroy");
+    //             this.Close();
+    //         }
+    // 
+    //     }
+
+
+    public override void OnNetMsg(byte[] pData)
+    {
+        Debug.Log("reTransform to other." + pData.Length);
+    }
+}
---
+++ unity_scripts/CELLSendStream.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine;
+
+public class CELLSendStream
+{
+    private List<byte> _byteList;
+
+    public CELLSendStream(int nCapacity = 128)
+    {
+        _byteList = new List<byte>(nCapacity);
+    }
+
+    //供其它写入基础数据类型调用
+    private void Write(byte[] data)
+    {
+        _byteList.AddRange(data);
+    }
+
+    public void WriteNetCMD(NetCMD netCMD)
+    {
+        WriteUInt16((UInt16)netCMD);
+    }
+
+    /// <summary>
+    /// 在字节序列最开始位置插入总的消息长度
+    /// </summary>
+    public void Finish()
+    {
+        if(_byteList.Count >= Int16.MaxValue)
+        {
+            Debug.Log("CELLSendStream len out of max range.");
+        }
+        UInt16 len = (UInt16)_byteList.Count;
+        //2 字节是长度导致的位置增加
+        len += 2;
+        _byteList.InsertRange(0, BitConverter.GetBytes(len));
+    }
+
+    /// <summary>
+    /// 转换成 byte[] 序列，方便传输
+    /// </summary>
+    /// <param name="n"></param>
+    public byte[] Array
+    {
+        get
+        {
+            return _byteList.ToArray();
+        }
+    }
+
+    //有符号
+    public void WriteInt8(sbyte n)
+    {
+        _byteList.Add((byte)n);
+    }
+
+    public void WriteInt16(Int16 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    public void WriteInt32(Int32 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    public void WriteInt64(Int64 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+    //无符号
+    public void WriteUInt8(byte n)
+    {
+        //Write(BitConverter.GetBytes(n));
+        _byteList.Add(n);
+    }
+
+    public void WriteUInt16(UInt16 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    public void WriteUInt32(UInt32 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    public void WriteUInt64(UInt64 n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+    //浮点数
+    public void WriteFloat(float n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    public void WriteDouble(double n)
+    {
+        Write(BitConverter.GetBytes(n));
+    }
+
+    //数组
+    /// <summary>
+    /// 数组类都需要写入元素个数
+    /// </summary>
+    /// <param name="data"></param>
+    public void WriteBytes(byte[] data)
+    {
+        //写入元素个数
+        WriteUInt32((UInt32)data.Length);
+        //写入具体的数据
+        Write(data);
+
+    }
+    /// <summary>
+    /// 字符串必须为 UTF-8 编码格式
+    /// 最后多加了一个结束符
+    /// </summary>
+    /// <param name="str"></param>
+    public void WriteString(string str)
+    {
+        //按照 UTF8 格式处理
+        byte[] data = Encoding.UTF8.GetBytes(str);
+        //写入元素个数
+        WriteUInt32((UInt32)data.Length + 1);
+        //写入具体的数据
+        Write(data);
+        WriteUInt8(0);
+    }
+
+    public void WriteInts(Int32[] data)
+    {
+        //写入元素个数
+        WriteUInt32((UInt32)data.Length);
+        //写入具体的数据
+        for(int n = 0; n < data.Length; n++)
+        {
+            WriteInt32(data[n]);
+        }        
+    }
+}
